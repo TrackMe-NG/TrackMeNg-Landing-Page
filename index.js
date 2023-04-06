02bc32adc5983520e927ee66c8ebdd39a415cdb4
Mozibix <opuzmoses@gmail.com>
2023-04-06 05:13:55 +0100

    chores: updating nav-toggle btn

---
+++ index.js
@@ -0,0 +1,29 @@
+const toogleBtn = document.querySelector(".toogle_btn");
+const toogleBtnIcon = document.querySelector(".toogle_btn i");
+const menu = document.querySelector(".dropdown_menu");
+
+/* NAV-FUNCTIONS */
+toogleBtn.onclick = () => {
+  menu.classList.toggle("open");
+
+  const isOpen = menu.classList.contains("open");
+
+  toogleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
+};
+
+const isClose = () => {
+  const isClose = menu.classList.remove("open");
+
+  toogleBtnIcon.classList = isClose ? "fa-solid fa-xmark" : "fa-solid fa-bars";
+};
+
+document.onclick = (e) => {
+  if (!toogleBtn.contains(e.target) && !menu.contains(e.target)) {
+    isClose();
+  }
+};
+document.onscroll = (e) => {
+  if (!toogleBtn.contains(e.target) && !menu.contains(e.target)) {
+    isClose();
+  }
+};
