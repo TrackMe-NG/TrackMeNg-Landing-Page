299f74c9ed6ab9876b84d0dc2658c3a1c8c226f5
Mozibix <opuzmoses@gmail.com>
2023-04-07 17:16:36 +0100

    chores: updating team_slides functionality

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const toogleBtn = document.querySelector(".toogle_btn");
 const toogleBtnIcon = document.querySelector(".toogle_btn i");
 const menu = document.querySelector(".dropdown_menu");
 
+/* TEAM SECTION */
+const slides = document.querySelectorAll(".slides");
+const dots = document.querySelectorAll(".dot");
+
+/* MORE TEXT VARIABLES */
+const moreDots = document.querySelector("#more_dots");
+const moreText = document.querySelector("#more");
+const btnText = document.querySelector("#more_btn");
+
 /* NAV-FUNCTIONS */
 toogleBtn.onclick = () => {
   menu.classList.toggle("open");
@@ -25,5 +34,50 @@ document.onclick = (e) => {
 document.onscroll = (e) => {
   if (!toogleBtn.contains(e.target) && !menu.contains(e.target)) {
     isClose();
+  }
+};
+
+/* TEAM SLIDER FUNCTIONS */
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+const plusSlides = (n) => {
+  showSlides((slideIndex += n));
+};
+
+const currentSlide = (n) => {
+  showSlides((slideIndex = n));
+};
+
+function showSlides(n) {
+  let i;
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace("active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+/* MORE TEXT FUNCTION */
+
+const readMore = () => {
+  if (moreDots.style.display === "none") {
+    moreDots.style.display = "inline";
+    btnText.innerHTML = "Learn more";
+    moreText.style.display = "none";
+  } else {
+    moreDots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
   }
 };
