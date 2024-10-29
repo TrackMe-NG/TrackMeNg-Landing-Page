206e2233ebdddec31e57f9343d53c326ed00e3ba
Mozibix <opuzmoses@gmail.com>
2024-10-29 20:07:48 +0100

    first commit

--- README.md
+++
@@ -1,2 +0,0 @@
-## TrackMeNg Web Page
-
--- index.js
+++ index.js
@@ -1,428 +1,96 @@
 /* PRE-LOADER FUNCTIONALITY*/
-document.addEventListener('DOMContentLoaded', () => {
-  const preloader = document.querySelector('#preloader_sec');
-  preloader.classList.remove('hidden');
-  preloader.classList.add('preloader');
+document.addEventListener("DOMContentLoaded", () => {
+  const preloader = document.querySelector("#preloader_sec");
+  preloader.classList.remove("hidden");
+  preloader.classList.add("preloader");
 
   void preloader.offsetWidth;
 
   setTimeout(() => {
-    preloader.classList.add('hidden');
+    preloader.classList.add("hidden");
   }, 1000);
 });
 
 /*  */
 
-const toogleBtn = document.querySelector('.toogle_btn');
-const toogleBtnIcon = document.querySelector('.toogle_btn i');
-const menu = document.querySelector('.dropdown_menu');
-const menuLists = document.querySelectorAll('.menu_lists li');
+const toogleBtn = document.querySelector(".toogle_btn");
+const toogleBtnIcon = document.querySelector(".toogle_btn i");
+const menu = document.querySelector(".dropdown_menu");
+const menuLists = document.querySelectorAll(".menu_lists li");
 
 /* TEAM SECTION */
-const slides = document.querySelectorAll('.slides');
-const dots = document.querySelectorAll('.dot');
+const slides = document.querySelectorAll(".slides");
+const dots = document.querySelectorAll(".dot");
 
 /* NAV-FUNCTIONS */
 
 /* ACTIVE LINKS */
 const removeActive = () => {
-  menuLists.forEach(link => {
-    link.classList.remove('active');
+  menuLists.forEach((link) => {
+    link.classList.remove("active");
   });
 };
 
-menuLists.forEach(link => {
-  link.addEventListener('click', () => {
+menuLists.forEach((link) => {
+  link.addEventListener("click", () => {
     removeActive();
-    link.classList.add('active');
+    link.classList.add("active");
   });
 });
 
 toogleBtn.onclick = () => {
-  menu.classList.toggle('open');
+  menu.classList.toggle("open");
 
-  const isOpen = menu.classList.contains('open');
+  const isOpen = menu.classList.contains("open");
 
-  toogleBtnIcon.classList = isOpen ? 'fa fa-times' : 'fa fa-bars';
+  toogleBtnIcon.classList = isOpen ? "fa fa-times" : "fa fa-bars";
 };
 
 const isClose = () => {
-  const isClose = menu.classList.remove('open');
+  const isClose = menu.classList.remove("open");
 
-  toogleBtnIcon.classList = isClose ? 'fa fa-times' : 'fa fa-bars';
+  toogleBtnIcon.classList = isClose ? "fa fa-times" : "fa fa-bars";
 };
 
-document.onclick = e => {
+document.onclick = (e) => {
   if (!toogleBtn.contains(e.target) && !menu.contains(e.target)) {
     isClose();
   }
 };
-document.onscroll = e => {
+document.onscroll = (e) => {
   if (!toogleBtn.contains(e.target) && !menu.contains(e.target)) {
     isClose();
   }
 };
 
-/* TEAM SLIDER FUNCTIONS */
+// Grab elements
+const video = document.getElementById("intro-video");
+const textSection = document.getElementById("text-section");
+const formSection = document.getElementById("form-section");
+const enterButton = document.getElementById("enter-button");
+const cancelButton = document.getElementById("cancel-button");
 
-let slideIndex = 1;
-showSlides(slideIndex);
+// Initially show text section, then delay the video load by 1 second
+setTimeout(() => {
+  textSection.style.display = "none"; // Hide text section
+  video.style.display = "block"; // Show video after 1 second delay
+  video.play(); // Ensure video starts playing
+}, 1000);
 
-const plusSlides = n => {
-  showSlides((slideIndex += n));
+// When the video ends, return to the text section
+video.onended = () => {
+  video.style.display = "none"; // Hide video after playing
+  textSection.style.display = "flex"; // Show the text and button again
 };
 
-const currentSlide = n => {
-  showSlides((slideIndex = n));
+// Show the form when "Enter Details" button is clicked
+enterButton.onclick = () => {
+  textSection.style.display = "none";
+  formSection.style.display = "flex";
 };
 
-function showSlides(n) {
-  let i;
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none';
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace('active', '');
-  }
-  slides[slideIndex - 1].style.display = 'block';
-  dots[slideIndex - 1].className += ' active';
-}
-
-/* MORE TEXT VARIABLES */
-const moreDots = document.querySelectorAll('.more_dots');
-const moreTexts = document.querySelectorAll('.more');
-const btnTexts = document.querySelectorAll('.more_btn');
-/* DESKTOP TEXT VARIABLES */
-const boxDots1 = document.querySelector('.box_dots1');
-const boxMore1 = document.querySelector('.box_more1');
-const boxMoreText1 = document.querySelector('.box_more_text1');
-/*  */
-const boxDots2 = document.querySelector('.box_dots2');
-const boxMore2 = document.querySelector('.box_more2');
-const boxMoreText2 = document.querySelector('.box_more_text2');
-/*  */
-const boxDots3 = document.querySelector('.box_dots3');
-const boxMore3 = document.querySelector('.box_more3');
-const boxMoreText3 = document.querySelector('.box_more_text3');
-/*  */
-const boxDots4 = document.querySelector('.box_dots4');
-const boxMore4 = document.querySelector('.box_more4');
-const boxMoreText4 = document.querySelector('.box_more_text4');
-/*  */
-const boxDots5 = document.querySelector('.box_dots5');
-const boxMore5 = document.querySelector('.box_more5');
-const boxMoreText5 = document.querySelector('.box_more_text5');
-/*  */
-const boxDots6 = document.querySelector('.box_dots6');
-const boxMore6 = document.querySelector('.box_more6');
-const boxMoreText6 = document.querySelector('.box_more_text6');
-/*  */
-// const boxDots = document.querySelectorAll(".box_dots");
-// const boxMore = document.querySelectorAll(".box_more");
-// const boxMoreText = document.querySelectorAll(".box_more_text");
-
-/* MORE TEXT FUNCTION */
-const readMore = () => {
-  moreDots.forEach(moreDot => {
-    moreTexts.forEach(moreText => {
-      btnTexts.forEach(btnText => {
-        if (moreDot.style.display === 'none') {
-          moreDot.style.display = 'inline';
-          btnText.innerHTML = 'Learn more';
-          moreText.style.display = 'none';
-        } else {
-          moreDot.style.display = 'none';
-          btnText.innerHTML = 'Read less';
-          moreText.style.display = 'inline';
-        }
-      });
-    });
-  });
+// Hide the form and go back to text section when "Cancel" button is clicked
+cancelButton.onclick = () => {
+  formSection.style.display = "none";
+  textSection.style.display = "flex";
 };
-
-btnTexts.forEach(btnText => {
-  btnText.addEventListener('click', readMore);
-});
-
-/* DESKTOP MORE TEXT FUNCTIONS */
-const boxReadMore1 = () => {
-  boxDots2.style.display = 'inline';
-  boxMoreText2.innerHTML = 'Learn more';
-  boxMore2.style.display = 'none';
-  /*  */
-  boxDots3.style.display = 'inline';
-  boxMoreText3.innerHTML = 'Learn more';
-  boxMore3.style.display = 'none';
-  /*  */
-  boxDots4.style.display = 'inline';
-  boxMoreText4.innerHTML = 'Learn more';
-  boxMore4.style.display = 'none';
-  /*  */
-  boxDots5.style.display = 'inline';
-  boxMoreText5.innerHTML = 'Learn more';
-  boxMore5.style.display = 'none';
-  /*  */
-  boxDots6.style.display = 'inline';
-  boxMoreText6.innerHTML = 'Learn more';
-  boxMore6.style.display = 'none';
-  /*  */
-  if (boxDots1.style.display === 'none') {
-    boxDots1.style.display = 'inline';
-    boxMoreText1.innerHTML = 'Learn more';
-    boxMore1.style.display = 'none';
-  } else {
-    boxDots1.style.display = 'none';
-    boxMoreText1.innerHTML = 'Read less';
-    boxMore1.style.display = 'inline';
-  }
-};
-/*  */
-const boxReadMore2 = () => {
-  boxDots1.style.display = 'inline';
-  boxMoreText1.innerHTML = 'Learn more';
-  boxMore1.style.display = 'none';
-  /*  */
-  boxDots3.style.display = 'inline';
-  boxMoreText3.innerHTML = 'Learn more';
-  boxMore3.style.display = 'none';
-  /*  */
-  boxDots4.style.display = 'inline';
-  boxMoreText4.innerHTML = 'Learn more';
-  boxMore4.style.display = 'none';
-  /*  */
-  boxDots5.style.display = 'inline';
-  boxMoreText5.innerHTML = 'Learn more';
-  boxMore5.style.display = 'none';
-  /*  */
-  boxDots6.style.display = 'inline';
-  boxMoreText6.innerHTML = 'Learn more';
-  boxMore6.style.display = 'none';
-  /*  */
-  if (boxDots2.style.display === 'none') {
-    boxDots2.style.display = 'inline';
-    boxMoreText2.innerHTML = 'Learn more';
-    boxMore2.style.display = 'none';
-  } else {
-    boxDots2.style.display = 'none';
-    boxMoreText2.innerHTML = 'Read less';
-    boxMore2.style.display = 'inline';
-  }
-};
-/*  */
-const boxReadMore3 = () => {
-  boxDots1.style.display = 'inline';
-  boxMoreText1.innerHTML = 'Learn more';
-  boxMore1.style.display = 'none';
-  /*  */
-  boxDots2.style.display = 'inline';
-  boxMoreText2.innerHTML = 'Learn more';
-  boxMore2.style.display = 'none';
-  /*  */
-  boxDots4.style.display = 'inline';
-  boxMoreText4.innerHTML = 'Learn more';
-  boxMore4.style.display = 'none';
-  /*  */
-  boxDots5.style.display = 'inline';
-  boxMoreText5.innerHTML = 'Learn more';
-  boxMore5.style.display = 'none';
-  /*  */
-  boxDots6.style.display = 'inline';
-  boxMoreText6.innerHTML = 'Learn more';
-  boxMore6.style.display = 'none';
-  /*  */
-  if (boxDots3.style.display === 'none') {
-    boxDots3.style.display = 'inline';
-    boxMoreText3.innerHTML = 'Learn more';
-    boxMore3.style.display = 'none';
-  } else {
-    boxDots3.style.display = 'none';
-    boxMoreText3.innerHTML = 'Read less';
-    boxMore3.style.display = 'inline';
-  }
-};
-/*  */
-const boxReadMore4 = () => {
-  boxDots1.style.display = 'inline';
-  boxMoreText1.innerHTML = 'Learn more';
-  boxMore1.style.display = 'none';
-  /*  */
-  boxDots2.style.display = 'inline';
-  boxMoreText2.innerHTML = 'Learn more';
-  boxMore2.style.display = 'none';
-  /*  */
-  boxDots3.style.display = 'inline';
-  boxMoreText3.innerHTML = 'Learn more';
-  boxMore3.style.display = 'none';
-  /*  */
-  boxDots5.style.display = 'inline';
-  boxMoreText5.innerHTML = 'Learn more';
-  boxMore5.style.display = 'none';
-  /*  */
-  boxDots6.style.display = 'inline';
-  boxMoreText6.innerHTML = 'Learn more';
-  boxMore6.style.display = 'none';
-  /*  */
-  if (boxDots4.style.display === 'none') {
-    boxDots4.style.display = 'inline';
-    boxMoreText4.innerHTML = 'Learn more';
-    boxMore4.style.display = 'none';
-  } else {
-    boxDots4.style.display = 'none';
-    boxMoreText4.innerHTML = 'Read less';
-    boxMore4.style.display = 'inline';
-  }
-};
-/*  */
-const boxReadMore5 = () => {
-  boxDots1.style.display = 'inline';
-  boxMoreText1.innerHTML = 'Learn more';
-  boxMore1.style.display = 'none';
-  /*  */
-  boxDots2.style.display = 'inline';
-  boxMoreText2.innerHTML = 'Learn more';
-  boxMore2.style.display = 'none';
-  /*  */
-  boxDots3.style.display = 'inline';
-  boxMoreText3.innerHTML = 'Learn more';
-  boxMore3.style.display = 'none';
-  /*  */
-  boxDots4.style.display = 'inline';
-  boxMoreText4.innerHTML = 'Learn more';
-  boxMore4.style.display = 'none';
-  /*  */
-  boxDots6.style.display = 'inline';
-  boxMoreText6.innerHTML = 'Learn more';
-  boxMore6.style.display = 'none';
-  /*  */
-  if (boxDots5.style.display === 'none') {
-    boxDots5.style.display = 'inline';
-    boxMoreText5.innerHTML = 'Learn more';
-    boxMore5.style.display = 'none';
-  } else {
-    boxDots5.style.display = 'none';
-    boxMoreText5.innerHTML = 'Read less';
-    boxMore5.style.display = 'inline';
-  }
-};
-/*  */
-const boxReadMore6 = () => {
-  boxDots1.style.display = 'inline';
-  boxMoreText1.innerHTML = 'Learn more';
-  boxMore1.style.display = 'none';
-  /*  */
-  boxDots2.style.display = 'inline';
-  boxMoreText2.innerHTML = 'Learn more';
-  boxMore2.style.display = 'none';
-  /*  */
-  boxDots3.style.display = 'inline';
-  boxMoreText3.innerHTML = 'Learn more';
-  boxMore3.style.display = 'none';
-  /*  */
-  boxDots4.style.display = 'inline';
-  boxMoreText4.innerHTML = 'Learn more';
-  boxMore4.style.display = 'none';
-  /*  */
-  boxDots5.style.display = 'inline';
-  boxMoreText5.innerHTML = 'Learn more';
-  boxMore5.style.display = 'none';
-  /*  */
-  if (boxDots6.style.display === 'none') {
-    boxDots6.style.display = 'inline';
-    boxMoreText6.innerHTML = 'Learn more';
-    boxMore6.style.display = 'none';
-  } else {
-    boxDots6.style.display = 'none';
-    boxMoreText6.innerHTML = 'Read less';
-    boxMore6.style.display = 'inline';
-  }
-};
-/*  */
-// const boxReadMore = () => {
-//   boxDots.forEach((boxDots) => {
-//     boxMore.forEach((boxMore) => {
-//       boxMoreText.forEach((boxMoreText) => {
-//         if (boxDots.style.display === "none") {
-//           boxDots.style.display = "inline";
-//           boxMoreText.innerHTML = "Learn more";
-//           boxMore.style.display = "none";
-//         } else {
-//           boxDots.style.display = "none";
-//           boxMoreText.innerHTML = "Read less";
-//           boxMore.style.display = "inline";
-//         }
-//       });
-//     });
-//   });
-// };
-
-// boxMoreText.forEach((boxMoreText) => {
-boxMoreText1.addEventListener('click', boxReadMore1);
-boxMoreText2.addEventListener('click', boxReadMore2);
-boxMoreText3.addEventListener('click', boxReadMore3);
-boxMoreText4.addEventListener('click', boxReadMore4);
-boxMoreText5.addEventListener('click', boxReadMore5);
-boxMoreText6.addEventListener('click', boxReadMore6);
-// });
-
-/* SCROLL FUNCTIONALITY */
-const animationUp = document.querySelectorAll('.animations_up');
-const animationDown = document.querySelectorAll('.animations_down');
-const animationLeft = document.querySelectorAll('.animations_left');
-const animationRight = document.querySelectorAll('.animations_right');
-const animationSpecial = document.querySelectorAll('.animations_left_special');
-const animation = document.querySelectorAll('.animations');
-
-const observer = new IntersectionObserver(
-  entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('scroll-animation');
-      } else {
-        entry.target.classList.remove('scroll-animation');
-      }
-    });
-  },
-  { threshold: 0.1 }
-);
-
-for (let i = 0; i < animationUp.length; i++) {
-  const elements = animationUp[i];
-
-  observer.observe(elements);
-}
-/*  */
-for (let i = 0; i < animationDown.length; i++) {
-  const elements = animationDown[i];
-
-  observer.observe(elements);
-}
-/*  */
-for (let i = 0; i < animationLeft.length; i++) {
-  const elements = animationLeft[i];
-
-  observer.observe(elements);
-}
-/*  */
-for (let i = 0; i < animationRight.length; i++) {
-  const elements = animationRight[i];
-
-  observer.observe(elements);
-}
-/*  */
-for (let i = 0; i < animationSpecial.length; i++) {
-  const elements = animationSpecial[i];
-
-  observer.observe(elements);
-}
-/*  */
-for (let i = 0; i < animation.length; i++) {
-  const elements = animation[i];
-
-  observer.observe(elements);
-}
